96fe694d0d8b54853a5ad956e1e7cf2a2a71431f
Nick <nikolay.galko@gmail.com>
2013-07-20 10:36:29 +0400

    Удалил Bugtrap,т.к он winonly и не смог внедрить-не работало Оставил только заготовку

--- Development/Src/Core/CoreManager.cpp
+++ Development/Src/Core/CoreManager.cpp
@@ -4,12 +4,14 @@
 #include "..\Common\ThreadPool.h"
 
 namespace VEGA {
+	void InitMiniDumper();
 
 	CoreManager::CoreManager()
 		: mTP(nullptr),
 		iWindow(nullptr),
 		iRender(nullptr),
 		running(false)	{
+			InitMiniDumper();
 			mTP=new ThreadPool(4);
 	}
 }
---
+++ Development/Src/Core/MiniDumper.cpp
@@ -0,0 +1,18 @@
+//TODO:Made this minidumper
+
+//**************************************
+#include "CorePrivate.h"
+#include <windows.h>
+//**************************************
+
+namespace VEGA {
+
+	//---------------------------------------------------------------------------
+	//Desc:    shows the error message box and exits
+	//Params:  text - text to show
+	//Returns: -
+	//---------------------------------------------------------------------------
+	void InitMiniDumper()
+	{
+	}
+}
