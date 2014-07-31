c7c564b0621ab946da31af2446fee5bdba0d8d86
Nick Galko <nick.galko@vegaengine.com>
2014-07-31 23:59:54 +0400

    [BUGFIX]VFile-параметр по умолчанию ILImage-Вернул прежнюю загрузку-была бажная

--- Development/Src/Core/src/ILImage.cpp
+++ Development/Src/Core/src/ILImage.cpp
@@ -1,10 +1,3 @@
-/***************************************************************************
-*   Copyright (C) 2006 by AST   *
-*   tsyplyaev@gmail.com   *
-*   ICQ: 279-533-134                          *
-*   This is a part of work done by AST.       *
-*   If you want to use it, please contact me. *
-***************************************************************************/
 #include "CorePrivate.h"
 //***************************************************************************
 #include "ILSystem.h"
@@ -18,24 +11,24 @@
 namespace NGTech {
 
 	int BPP2Format(int bpp) {
-		if(bpp == 3) return ILImage::RGB;
-		if(bpp == 4) return ILImage::RGBA;
+		if (bpp == 3) return ILImage::RGB;
+		if (bpp == 4) return ILImage::RGBA;
 		return 0;
 	}
 
 	int Format2Bpp(int format) {
-		if(format == ILImage::RGB) return 3;
-		if(format == ILImage::RGBA) return 4;
+		if (format == ILImage::RGB) return 3;
+		if (format == ILImage::RGBA) return 4;
 		return 0;
 	}
 
 	ILImage *ILImage::createEmpty2d(int width, int height, int format) {
 		ILImage *image = new ILImage();
 
-		image->width  = width;
+		image->width = width;
 		image->height = height;
-		image->depth  = 1;
-		image->bpp    = Format2Bpp(format);
+		image->depth = 1;
+		image->bpp = Format2Bpp(format);
 		image->format = format;
 		image->data = new ILubyte[image->getSize()];
 
@@ -45,15 +38,15 @@ namespace NGTech {
 	ILImage *ILImage::createNoise2d(int width, int height, int format) {
 		ILImage *image = new ILImage();
 
-		image->width  = width;
+		image->width = width;
 		image->height = height;
-		image->depth  = 1;
-		image->bpp    = Format2Bpp(format);
+		image->depth = 1;
+		image->bpp = Format2Bpp(format);
 		image->format = format;
 		image->data = new ILubyte[image->getSize()];
 
-		for(int i = 0; i < image->getSize(); i++) {
-			image->data[i] = (ILubyte)rand()%255;
+		for (int i = 0; i < image->getSize(); i++) {
+			image->data[i] = (ILubyte)rand() % 255;
 		}
 
 		return image;
@@ -62,10 +55,10 @@ namespace NGTech {
 	ILImage *ILImage::createEmpty3d(int width, int height, int depth, int format) {
 		ILImage *image = new ILImage();
 
-		image->width  = width;
+		image->width = width;
 		image->height = height;
-		image->depth  = depth;
-		image->bpp    = Format2Bpp(format);
+		image->depth = depth;
+		image->bpp = Format2Bpp(format);
 		image->format = format;
 		image->data = new ILubyte[image->getSize()];
 
@@ -75,15 +68,15 @@ namespace NGTech {
 	ILImage *ILImage::createNoise3d(int width, int height, int depth, int format) {
 		ILImage *image = new ILImage();
 
-		image->width  = width;
+		image->width = width;
 		image->height = height;
-		image->depth  = depth;
-		image->bpp    = Format2Bpp(format);
+		image->depth = depth;
+		image->bpp = Format2Bpp(format);
 		image->format = format;
 		image->data = new ILubyte[image->getSize()];
 
-		for(int i = 0; i < image->getSize(); i++) {
-			image->data[i] = (ILubyte)rand()%255;
+		for (int i = 0; i < image->getSize(); i++) {
+			image->data[i] = (ILubyte)rand() % 255;
 		}
 
 		return image;
@@ -91,42 +84,44 @@ namespace NGTech {
 
 	ILImage *ILImage::create2d(const String &_path) {
 		ILImage *image = new ILImage();
+
 		VFile mFile(_path.c_str(), VFile::READ_BIN);
 		String ext = mFile.GetFileExt();
-		unsigned char *buf = (unsigned char*)mFile.LoadFile();
-		unsigned int size = (unsigned int)mFile.Size();
+		auto path = mFile.GetDataPath();
+
+		ilOriginFunc(IL_ORIGIN_UPPER_LEFT);
 
 		if (ext == "bmp")
-			ilLoadL(IL_BMP, buf, size);
+			ilLoad(IL_BMP, (const ILstring)path);
 		if (ext == "tga")
-			ilLoadL(IL_TGA, buf, size);
+			ilLoad(IL_TGA, (const ILstring)path);
 		if (ext == "jpg")
-			ilLoadL(IL_JPG, buf, size);
-		if(ext == "png")
-			ilLoadL(IL_PNG, buf, size);
-		if(ext == "dds")
-			ilLoadL(IL_DDS, buf, size);
-		if(ext == "gif")
-			ilLoadL(IL_GIF, buf, size);
+			ilLoad(IL_JPG, (const ILstring)path);
+		if (ext == "png")
+			ilLoad(IL_PNG, (const ILstring)path);
+		if (ext == "dds")
+			ilLoad(IL_DDS, (const ILstring)path);
+		if (ext == "gif")
+			ilLoad(IL_GIF, (const ILstring)path);
 
 		int error = ilGetError();
-		if(error != IL_NO_ERROR) {
-			String errorStr = (const char *) iluErrorString(error);
-			Log::write("ILImage::create2d() error: " + errorStr);
+		if (error != IL_NO_ERROR) {
+			String errorStr = (const char *)iluErrorString(error);
+			Warning("ILImage::Create2d() error: %s" ,errorStr);
 			return NULL;
-		}
+		};
 
-		image->width  = ilGetInteger(IL_IMAGE_WIDTH);
+		image->width = ilGetInteger(IL_IMAGE_WIDTH);
 		image->height = ilGetInteger(IL_IMAGE_HEIGHT);
-		image->depth  = 1;
-		image->bpp    = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
+		image->depth = 1;
+		image->bpp = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
 		image->format = BPP2Format(image->bpp);
 
 		ILubyte *tempData = ilGetData();
-
 		image->data = new ILubyte[image->getSize()];
 
 		memcpy(image->data, tempData, image->getSize());
+
 		return image;
 	}
 
@@ -137,43 +132,43 @@ namespace NGTech {
 	void ILImage::toNormalMap(int k) {
 		int w = width;
 		int h = height;
-		if(depth > 1) 
+		if (depth > 1)
 			return;
 
 		int byteCount = w*h;
 		ILubyte* ndata = new ILubyte[byteCount];
 
-		for(int i = 0; i < byteCount; i++) {
-			ndata[i] = (data[ i*bpp ]  + data[i*bpp + 1] + data[i*bpp + 2]) / 3.0;
+		for (int i = 0; i < byteCount; i++) {
+			ndata[i] = (data[i*bpp] + data[i*bpp + 1] + data[i*bpp + 2]) / 3.0;
 		}
 
 		float oneOver255 = 1.0 / 255.0;
-		int   offs = 0; 
+		int   offs = 0;
 
-		for(int i = 0; i < w; i++)
+		for (int i = 0; i < w; i++)
 		{
-			for(int j = 0; j < h; j++)
+			for (int j = 0; j < h; j++)
 			{
-				float c  = ndata[i*h + j        ] * oneOver255;
-				float cx = ndata[i*h + (j+1)%w  ] * oneOver255;
-				float cy = ndata[((i+1)%h)*h + j] * oneOver255;
+				float c = ndata[i*h + j] * oneOver255;
+				float cx = ndata[i*h + (j + 1) % w] * oneOver255;
+				float cy = ndata[((i + 1) % h)*h + j] * oneOver255;
 
 				float dx = (c - cx) * k;
 				float dy = (c - cy) * k;
 
-				float len = (float) sqrt(dx*dx + dy*dy + 1);
+				float len = (float)sqrt(dx*dx + dy*dy + 1);
 
 				float nx = dy / len;
 				float ny = -dx / len;
 				float nz = 1.0 / len;
 
-				data[ offs ]     = (ILubyte)(128 + 127*nx);
-				data[ offs + 1 ] = (ILubyte)(128 + 127*ny);
-				data[ offs + 2 ] = (ILubyte)(128 + 127*nz);
+				data[offs] = (ILubyte)(128 + 127 * nx);
+				data[offs + 1] = (ILubyte)(128 + 127 * ny);
+				data[offs + 2] = (ILubyte)(128 + 127 * nz);
 				offs += bpp;
 			}
 		}
-		delete [] ndata;
+		delete[] ndata;
 	}
 
 	void ILImage::toGreyScale() {
@@ -181,15 +176,15 @@ namespace NGTech {
 		int h = height;
 		int d = depth;
 
-		for(int i = 0; i < w*h*d; i++)
+		for (int i = 0; i < w*h*d; i++)
 		{
-			ILubyte color = (data[ i*bpp ] 
-			+ data[ i*bpp + 1 ] 
-			+ data[ i*bpp + 2 ]) / 3.0;
+			ILubyte color = (data[i*bpp]
+				+ data[i*bpp + 1]
+				+ data[i*bpp + 2]) / 3.0;
 
-			data[ i*bpp ] = color;
-			data[ i*bpp + 1 ] = color;
-			data[ i*bpp + 2 ] = color;
+			data[i*bpp] = color;
+			data[i*bpp + 1] = color;
+			data[i*bpp + 2] = color;
 		}
 	}
 
